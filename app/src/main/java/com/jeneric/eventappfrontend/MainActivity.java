83ddf8997ec7f8ae6d609fe6e7e953d63ac2af42
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-09-30 21:10:57 +0100

    Feat: Implement replaceFragment Method

--- app/src/main/java/com/jeneric/eventappfrontend/MainActivity.java
+++ app/src/main/java/com/jeneric/eventappfrontend/MainActivity.java
@@ -29,5 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
 }
