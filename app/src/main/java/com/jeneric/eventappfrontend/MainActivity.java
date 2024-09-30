258bea83945f66ec9648ff12e4bfa306e17b16c3
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-09-30 21:10:20 +0100

    Feat: Implement binding in main activity

--- app/src/main/java/com/jeneric/eventappfrontend/MainActivity.java
+++ app/src/main/java/com/jeneric/eventappfrontend/MainActivity.java
@@ -7,18 +7,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.jeneric.eventappfrontend.databinding.ActivityMainBinding;
+import com.jeneric.eventappfrontend.ui.create.CreateFragment;
+import com.jeneric.eventappfrontend.ui.explore.ExploreFragment;
+import com.jeneric.eventappfrontend.ui.home.HomeFragment;
+import com.jeneric.eventappfrontend.ui.settings.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
+
 }
