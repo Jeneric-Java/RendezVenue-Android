e5e698200d8ef33cf75e970f3069f47d6c19aea3
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-10-01 10:55:04 +0100

    Feat: add ClickHandler class

---
+++ app/src/main/java/com/jeneric/eventappfrontend/ui/main/MainActivityClickHandlers.java
@@ -0,0 +1,15 @@
+package com.jeneric.eventappfrontend.ui.main;
+
+import android.content.Context;
+
+public class MainActivityClickHandlers {
+
+    Context context;
+
+    public MainActivityClickHandlers(Context context) {
+        this.context = context;
+    }
+
+
+
+}
