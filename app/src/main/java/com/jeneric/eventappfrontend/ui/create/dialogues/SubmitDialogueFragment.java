26aa1159d5d667cbf10a9f8a7f7e7e7b36c0e730
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-10-05 13:44:12 +0100

    Feat: implement click handler to button

--- app/src/main/java/com/jeneric/eventappfrontend/ui/create/dialogues/SubmitDialogueFragment.java
+++ app/src/main/java/com/jeneric/eventappfrontend/ui/create/dialogues/SubmitDialogueFragment.java
@@ -1,8 +1,10 @@
 package com.jeneric.eventappfrontend.ui.create.dialogues;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
@@ -11,8 +13,9 @@ import com.jeneric.eventappfrontend.R;
 
 public class SubmitDialogueFragment extends DialogFragment {
 
+    @NonNull
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
 
         builder.setMessage(R.string.add_event_to_cal);
@@ -28,6 +31,6 @@ public class SubmitDialogueFragment extends DialogFragment {
 
             }
         });
-           builder.create();
+           return builder.create();
     }
 }
