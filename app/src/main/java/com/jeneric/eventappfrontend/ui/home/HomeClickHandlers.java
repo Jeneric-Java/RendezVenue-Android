da20e87a17b7df1c664418d07024636c9d275777
Joeltt <joelttm15@gmail.com>
2024-10-06 17:13:21 +0100

    feat: Enable search bar in home fragment to open explore fragment

--- app/src/main/java/com/jeneric/eventappfrontend/ui/home/HomeClickHandlers.java
+++ app/src/main/java/com/jeneric/eventappfrontend/ui/home/HomeClickHandlers.java
@@ -1,13 +1,30 @@
 package com.jeneric.eventappfrontend.ui.home;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.View;
+import android.widget.SearchView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.jeneric.eventappfrontend.R;
+import com.jeneric.eventappfrontend.databinding.FragmentHomeBinding;
 
 public class HomeClickHandlers {
 
+    private NavController navController;
     Context context;
 
-    public HomeClickHandlers(Context context) {
+    public HomeClickHandlers(NavController navController, Context context) {
+        this.navController = navController;
         this.context = context;
+    }
+
+    public void onSearchBarClick(View view) {
+        navController.navigate(R.id.exploreFragment);
+        NavController navController = Navigation.findNavController((Activity) context, R.id.nav_host_fragment_container);
+
     }
 
 }
