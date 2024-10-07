d131bd71c3caea9fdc386f20b89ceb77da9de387
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-10-07 10:19:52 +0100

    Refactor: refactor TimeModel class

--- app/src/main/java/com/jeneric/eventappfrontend/model/TimeModel.java
+++ app/src/main/java/com/jeneric/eventappfrontend/model/TimeModel.java
@@ -12,21 +12,17 @@ import com.jeneric.eventappfrontend.BR;
 
 public class TimeModel extends BaseObservable implements Parcelable {
 
-    @SerializedName("id")
-    private Long id;
-    @SerializedName("year")
     private int year;
-    @SerializedName("month")
+
     private int month;
-    @SerializedName("dayOfMonth")
+
     private int dayOfMonth;
-    @SerializedName("hourOfDay")
+
     private int hourOfDay;
-    @SerializedName("minute")
+
     private int minute;
 
-    public TimeModel(Long id, int year, int month, int dayOfMonth, int hourOfDay, int minute) {
-        this.id = id;
+    public TimeModel(int year, int month, int dayOfMonth, int hourOfDay, int minute) {
         this.year = year;
         this.month = month;
         this.dayOfMonth = dayOfMonth;
@@ -35,7 +31,6 @@ public class TimeModel extends BaseObservable implements Parcelable {
     }
 
     protected TimeModel(Parcel in) {
-        id = in.readLong();
         year = in.readInt();
         month = in.readInt();
         dayOfMonth = in.readInt();
@@ -55,64 +50,45 @@ public class TimeModel extends BaseObservable implements Parcelable {
         }
     };
 
-    @Bindable
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-        notifyPropertyChanged(BR.id);
-    }
-
-    @Bindable
     public int getYear() {
         return year;
     }
 
     public void setYear(int year) {
         this.year = year;
-        notifyPropertyChanged(BR.year);
     }
 
-    @Bindable
+
     public int getMonth() {
         return month;
     }
 
     public void setMonth(int month) {
         this.month = month;
-        notifyPropertyChanged(BR.month);
     }
 
-    @Bindable
     public int getDayOfMonth() {
         return dayOfMonth;
     }
 
     public void setDayOfMonth(int dayOfMonth) {
         this.dayOfMonth = dayOfMonth;
-        notifyPropertyChanged(BR.dayOfMonth);
     }
 
-    @Bindable
     public int getHourOfDay() {
         return hourOfDay;
     }
 
     public void setHourOfDay(int hourOfDay) {
         this.hourOfDay = hourOfDay;
-        notifyPropertyChanged(BR.hourOfDay);
     }
 
-    @Bindable
     public int getMinute() {
         return minute;
     }
 
     public void setMinute(int minute) {
         this.minute = minute;
-        notifyPropertyChanged(BR.minute);
     }
 
     @Override
@@ -122,7 +98,6 @@ public class TimeModel extends BaseObservable implements Parcelable {
 
     @Override
     public void writeToParcel(@NonNull Parcel dest, int flags) {
-        dest.writeLong(id);
         dest.writeInt(year);
         dest.writeInt(month);
         dest.writeInt(dayOfMonth);
